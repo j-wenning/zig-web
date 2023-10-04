f760bd545d10130c7e43f5e38c9ad01740d02e3e
j-wenning <wenning.justin@gmail.com>
2023-10-04 00:22:19 -0700

    Routing and Slice adjustments
    * Remove allocator requirement for path node initialization
    
    * Propagate memory errors from route additions
    
    * Add route tree traversal
    
    * Use pass by value when appending to slices

--- src/route.zig
+++ src/route.zig
@@ -60,12 +60,12 @@ const RouteNode = struct {
     middlewares: []RouteMiddleware,
     handlers: []RouteHandler,
 
-    fn init(ident: RouteIdent, allocator: std.mem.Allocator) Self {
+    fn init(ident: RouteIdent) Self {
         return Self{
             .ident = ident,
-            .children = allocator.alloc(Self, 0) catch unreachable,
-            .middlewares = allocator.alloc(RouteMiddleware, 0) catch unreachable,
-            .handlers = allocator.alloc(RouteHandler, 0) catch unreachable,
+            .children = &[_]Self{},
+            .middlewares = &[_]RouteMiddleware{},
+            .handlers = &[_]RouteHandler{},
         };
     }
 
@@ -78,7 +78,7 @@ const RouteNode = struct {
         allocator.free(self.handlers);
     }
 
-    fn add(root: *Self, path: []const u8, value: RouteNodeValue, allocator: std.mem.Allocator) void {
+    fn add(root: *Self, allocator: std.mem.Allocator, path: []const u8, value: RouteNodeValue) !void {
         var path_segment_iter = std.mem.splitScalar(u8, path, path_separator);
 
         var node = root;
@@ -88,12 +88,12 @@ const RouteNode = struct {
                 if (RouteIdent.cmp(&child.ident, &ident)) {
                     break @constCast(child);
                 }
-            } else slice.append(Self, allocator, &node.children, &Self.init(ident, allocator)) catch unreachable;
+            } else try slice.append(Self, allocator, &node.children, Self.init(ident));
         }
 
         switch (value) {
-            .middleware => |middleware| _ = slice.append(RouteMiddleware, allocator, &node.middlewares, &middleware) catch unreachable,
-            .handler => |handler| _ = slice.append(RouteHandler, allocator, &node.handlers, &handler) catch unreachable,
+            .middleware => |middleware| _ = try slice.append(RouteMiddleware, allocator, &node.middlewares, middleware),
+            .handler => |handler| _ = try slice.append(RouteHandler, allocator, &node.handlers, handler),
         }
     }
 };
@@ -107,7 +107,7 @@ pub const RouteTree = struct {
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{
             .allocator = allocator,
-            .root = RouteNode.init(RouteIdent{ .name = "/" }, allocator),
+            .root = RouteNode.init(RouteIdent{ .name = "/" }),
         };
     }
 
@@ -115,12 +115,40 @@ pub const RouteTree = struct {
         self.root.deinit(self.allocator);
     }
 
-    pub fn addMiddleware(self: *Self, path: []const u8, middleware: RouteMiddleware) void {
-        RouteNode.add(&self.root, path, RouteNodeValue{ .middleware = middleware }, self.allocator);
+    pub fn addMiddleware(self: *Self, path: []const u8, middleware: RouteMiddleware) !void {
+        try RouteNode.add(&self.root, self.allocator, path, RouteNodeValue{ .middleware = middleware });
     }
 
-    pub fn addHandler(self: *Self, method: std.http.Method, path: []const u8, handler: RouteMiddleware) void {
-        RouteNode.add(&self.root, path, RouteNodeValue{ .handler = .{ .method = method, .middleware = handler } }, self.allocator);
+    pub fn addHandler(self: *Self, method: std.http.Method, path: []const u8, handler: RouteMiddleware) !void {
+        try RouteNode.add(&self.root, self.allocator, path, RouteNodeValue{ .handler = .{ .method = method, .middleware = handler } });
+    }
+
+    pub fn collect(
+        self: *Self,
+        method: std.http.Method,
+        path: []const u8,
+        middleware_accumulator: *std.ArrayList(RouteMiddleware),
+        param_accumulator: *std.ArrayList([]const u8),
+    ) !void {
+        var segments = std.mem.splitScalar(u8, path, path_separator);
+        var node = &self.root;
+        while (segments.next()) |segment| {
+            node = for (node.children) |*child| {
+                switch (child.ident) {
+                    .name => |name| if (!std.mem.eql(u8, name, segment)) continue,
+                    .param => try param_accumulator.append(segment),
+                }
+                try middleware_accumulator.appendSlice(child.middlewares);
+                break child;
+            } else return error.RouteNotFound;
+        }
+        const handler = for (node.handlers) |*handler| {
+            if (handler.method == method) {
+                break handler.*.middleware;
+            }
+        } else return error.RouteNotFound;
+
+        try middleware_accumulator.append(handler);
     }
 };
 
@@ -132,19 +160,144 @@ test "route allocations should not leak" {
     var tree = RouteTree.init(std.testing.allocator);
     defer tree.deinit();
 
-    tree.addHandler(.GET, "foo", mockMiddleware);
-    tree.addHandler(.GET, "foo", mockMiddleware);
-    tree.addHandler(.GET, "bar", mockMiddleware);
-    tree.addHandler(.GET, "foo/bar", mockMiddleware);
-    tree.addHandler(.GET, "foo/bar/baz", mockMiddleware);
-    tree.addHandler(.GET, "foo/bar", mockMiddleware);
-    tree.addHandler(.GET, "bar/baz", mockMiddleware);
+    try tree.addHandler(.GET, "foo", mockMiddleware);
+    try tree.addHandler(.GET, "foo", mockMiddleware);
+    try tree.addHandler(.GET, "bar", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar/baz", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar", mockMiddleware);
+    try tree.addHandler(.GET, "bar/baz", mockMiddleware);
 
-    tree.addMiddleware("foo", mockMiddleware);
-    tree.addMiddleware("foo", mockMiddleware);
-    tree.addMiddleware("bar", mockMiddleware);
-    tree.addMiddleware("foo/bar", mockMiddleware);
-    tree.addMiddleware("foo/bar/baz", mockMiddleware);
-    tree.addMiddleware("foo/bar", mockMiddleware);
-    tree.addMiddleware("bar/baz", mockMiddleware);
+    try tree.addMiddleware("foo", mockMiddleware);
+    try tree.addMiddleware("foo", mockMiddleware);
+    try tree.addMiddleware("bar", mockMiddleware);
+    try tree.addMiddleware("foo/bar", mockMiddleware);
+    try tree.addMiddleware("foo/bar/baz", mockMiddleware);
+    try tree.addMiddleware("foo/bar", mockMiddleware);
+    try tree.addMiddleware("bar/baz", mockMiddleware);
+}
+
+test "traversal returns expected result" {
+    var tree = RouteTree.init(std.testing.allocator);
+    defer tree.deinit();
+
+    var macc = std.ArrayList(RouteMiddleware).init(std.testing.allocator);
+    defer macc.deinit();
+
+    var pacc = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer pacc.deinit();
+
+    try tree.addHandler(.GET, "foo", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar/baz", mockMiddleware);
+
+    try tree.collect(.GET, "foo", &macc, &pacc);
+
+    try tree.collect(.GET, "foo/bar/baz", &macc, &pacc);
+
+    try std.testing.expectError(
+        error.RouteNotFound,
+        tree.collect(.GET, "bar", &macc, &pacc),
+    );
+
+    try std.testing.expectError(
+        error.RouteNotFound,
+        tree.collect(.GET, "foo/a", &macc, &pacc),
+    );
+
+    try std.testing.expectError(
+        error.RouteNotFound,
+        tree.collect(.GET, "foo/bar/a", &macc, &pacc),
+    );
+}
+
+test "traversal results in expected collections of middleware" {
+    var tree = RouteTree.init(std.testing.allocator);
+    defer tree.deinit();
+
+    var macc = std.ArrayList(RouteMiddleware).init(std.testing.allocator);
+    defer macc.deinit();
+
+    var pacc = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer pacc.deinit();
+
+    // Pure handlers
+    try tree.addHandler(.GET, "foo", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar/baz", mockMiddleware);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 1);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 1);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar/baz", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 1);
+
+    // Handlers with middleware
+    try tree.addMiddleware("foo/bar", mockMiddleware);
+    try tree.addMiddleware("foo", mockMiddleware);
+    try tree.addMiddleware("foo/bar/baz", mockMiddleware);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 2);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 3);
+
+    macc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar/baz", &macc, &pacc);
+    try std.testing.expect(macc.items.len == 4);
+}
+
+test "traversal results in expected collections of params" {
+    var tree = RouteTree.init(std.testing.allocator);
+    defer tree.deinit();
+
+    var macc = std.ArrayList(RouteMiddleware).init(std.testing.allocator);
+    defer macc.deinit();
+
+    var pacc = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer pacc.deinit();
+
+    try tree.addHandler(.GET, "foo/bar/baz", mockMiddleware);
+    try tree.addHandler(.GET, "foo/bar/*", mockMiddleware);
+    try tree.addHandler(.GET, "foo/*/baz", mockMiddleware);
+    try tree.addHandler(.GET, "*/bar/baz", mockMiddleware);
+    try tree.addHandler(.GET, "foo/*/*", mockMiddleware);
+    try tree.addHandler(.GET, "*/*/baz", mockMiddleware);
+    try tree.addHandler(.GET, "*/*/*", mockMiddleware);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar/baz", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 0);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/bar/a", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 1);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/a/baz", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 1);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "a/bar/baz", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 1);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "foo/a/a", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 2);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "a/a/baz", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 2);
+
+    pacc.clearRetainingCapacity();
+    try tree.collect(.GET, "a/a/a", &macc, &pacc);
+    try std.testing.expect(pacc.items.len == 3);
 }
--- src/slice.zig
+++ src/slice.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-pub fn append(comptime T: type, allocator: std.mem.Allocator, slice: *[]T, value: *const T) !*T {
+pub fn append(comptime T: type, allocator: std.mem.Allocator, slice: *[]T, value: T) !*T {
     const new_idx = slice.len;
     const new_len = slice.len + 1;
     const new = try allocator.realloc(slice.*, new_len);
-    new[new_idx] = value.*;
+    new[new_idx] = value;
     slice.* = new;
     return &new[new_idx];
 }
